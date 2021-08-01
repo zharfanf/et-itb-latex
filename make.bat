04d2d38fce8b63800ae7ef0031e521d9e9308cc6
titoirfan <irfantitok@gmail.com>
2021-08-01 22:11:35 +0700

    Repo cleanup

--- make.bat
+++
@@ -1,9 +0,0 @@
-REM clean
-rmdir /S /Q build
-rmdir /S /Q output
-	
-REM install
-mkdir -p output
-mkdir -p build
-latexmk -pdf -bibtex -outdir=../build -cd src/thesis.tex
-move build\thesis.pdf output
